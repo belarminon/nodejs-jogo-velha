b3ac82346764361245008d9e8aa66840868fa833
Belarmino Nicolau Monteiro Simão <belarnicolau@hotmail.com>
2022-09-28 01:15:46 -0300

    initial commit for all project.

---
+++ dist/scripts.js
@@ -0,0 +1,127 @@
+
+window.onload = () => {
+    new JogoVelha();
+};
+
+class JogoVelha {
+
+    constructor() {
+        this.iniciaElementos();
+        this.iniciaEstado();
+    }
+
+    iniciaEstado() {
+        this.turno = true;
+        this.jogadas = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        this.fim = false;
+        this.vitoria = [448, 56, 7, 292, 146, 73, 273, 84];
+    }
+
+    iniciaElementos() {
+
+        this.jogadorX = document.querySelector('#jogador-x');
+        this.jogadorO = document.querySelector('#jogador-o');
+
+        this.salvarLocal = document.querySelector('#salva-local');
+        this.salvarLocal.addEventListener('click', this.salvaLocal.bind(this));
+
+        this.carregarLocal = document.querySelector('#carrega-local');
+        this.carregarLocal.addEventListener('click', this.carregaLocal.bind(this));
+
+        this.limparLocal = document.querySelector('#limpa-local');
+        this.limparLocal.addEventListener('click', this.limpaLocal.bind(this));
+
+        this.velha = document.querySelector('#velha');
+        this.velha.addEventListener('click', (event) => {
+            this.realizaJogada(event);
+            this.render();
+        });
+    }
+
+    salvaLocal() {
+        const dados = {
+            jogadorX: this.jogadorX.value,
+            jogadorO: this.jogadorO.value,
+            jogadas: this.jogadas
+        }
+        localStorage.setItem('jogo', JSON.stringify(dados));
+    }
+
+    carregaLocal() {
+        const dados = JSON.parse(localStorage.getItem('jogo'));
+
+        if (dados.length != 0) {
+
+            this.jogadorX.value = dados.jogadorX;
+            this.jogadorO.value = dados.jogadorO;
+            this.jogadas = dados.jogadas;
+
+            this.render();
+        }
+
+
+    }
+
+    limpaLocal() {
+        localStorage.removeItem('jogo');
+        this.jogadorX.value = '';
+        this.jogadorO.value = '';
+
+        this.iniciaEstado();
+
+        this.render();
+    }
+
+    realizaJogada(event) {
+
+        const id = event.target.dataset.id;
+
+        if (this.fim) {
+            console.log("Partida terminada");
+            return;
+        }
+
+        this.jogadas[id] = this.turno ? 'X' : 'O';
+        this.turno = !this.turno;
+    }
+
+    render() {
+
+        var resultado = this.verificaVitoria();
+        if (resultado == 'X' || resultado == 'O') {
+            this.fim = true;
+            console.log(`O jogador ${resultado} venceu!`);
+        }
+        
+        //iterar os elementos no tabuleiro
+        var velhaElemento = document.querySelectorAll('[data-id]');
+
+        for (let i = 0; i < velhaElemento.length; i++) {
+            velhaElemento[i].innerHTML = this.jogadas[i] == 0 ? "" : this.jogadas[i];
+        }
+    }
+
+    verificaVitoria() {
+
+        //deciml da sequencia de quem jogou X
+        var valorX = parseInt(this.jogadas.map(value => value == 'X' ? 1 : 0).join(""), 2);
+
+        //deciml da sequencia de quem jogou y
+        var valorO = parseInt(this.jogadas.map(value => value == 'O' ? 1 : 0).join(""), 2);
+
+        //percorrer array vitoria perguntando
+        for (const element of this.vitoria) {
+
+            if ((element & valorX) == element) {
+                return "X";
+            }
+
+            if ((element & valorO) == element) {
+                return "O";
+            }
+
+            return "";
+        }
+    }
+
+}
